417764b193636219dfbd7fefee0a31a8efbc8e98
MaxBlach <maxime.blachere@outlook.fr>
2020-12-04 00:17:42 +0100

    Fin de l'implementation 2

--- src/spots/spots.service.ts
+++ src/spots/spots.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { CreateSpotsDto } from './dto/create-spots.dto';
+import { Spots } from "./spots.entity";
 
 @Injectable()
-export class SpotsService {}
+export class SpotsService {
+    async create(dto: CreateSpotsDto) {
+        const report = Spots.fromDto(dto);
+        return await report.save();
+    }
+}
