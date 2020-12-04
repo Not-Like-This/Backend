066f16cc230b3aa0fcbaccc3162c634c35db24b0
Julien Lavocat <julien.lavocat@gmail.com>
2020-12-04 03:43:10 +0100

    Fixed code indentation

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -1,37 +1,35 @@
-import { Entity, PrimaryGeneratedColumn, BaseEntity, Column} from "typeorm";
+import { Entity, PrimaryGeneratedColumn, BaseEntity, Column } from "typeorm";
 import { CreateUsersDto } from "./dto/create-users.dto";
 
 @Entity()
 export class Users extends BaseEntity {
-    @PrimaryGeneratedColumn()
-    id: number;
+	@PrimaryGeneratedColumn()
+	id: number;
 
-    //Info compte
-    @Column()
-    mail: string;
+	//Info compte
+	@Column()
+	mail: string;
 
-    @Column()
-    password: string;
+	@Column()
+	password: string;
 
-    //Info User
-    @Column()
-    name: string;
+	//Info User
+	@Column()
+	name: string;
 
-    @Column()
-    city: string;
+	@Column()
+	city: string;
 
+	//TODO: Others
 
-    //TODO: Others
+	static fromDto(dto: CreateUsersDto) {
+		const e = new Users();
 
-    static fromDto(dto: CreateUsersDto) {
-        const e = new Users();
+		e.mail = dto.mail;
+		e.password = dto.password;
+		e.name = dto.name;
+		e.city = dto.city;
 
-        e.mail = dto.mail;
-        e.password = dto.password;
-        e.name = dto.name;
-        e.city = dto.city;
-
-        return e;
-    }
-
+		return e;
+	}
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from "@nestjs/common";
-import { CreateUsersDto } from './dto/create-users.dto';
-import { Users } from './users.entity';
+import { CreateUsersDto } from "./dto/create-users.dto";
+import { Users } from "./users.entity";
 
 @Injectable()
 export class UsersService {
-    async create(dto: CreateUsersDto){
-        const report = Users.fromDto(dto);
-        return await report.save();
-    }
+	async create(dto: CreateUsersDto) {
+		const report = Users.fromDto(dto);
+		return await report.save();
+	}
 }
